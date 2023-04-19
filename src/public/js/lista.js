67a387b71d55d09a569d1bfdaee6ed537da8b511
jorge-rubinich <jr@bitbyte.com.ar>
2023-04-18 23:00:18 -0300

    Entrega handlebars-socket - Casi terminado. Faltan detalles

--- src/public/js/lista.js
+++ src/public/js/lista.js
@@ -1,13 +1,19 @@
 const socket = io()
 socket.emit('connection')
 
+let productBuffer=[]
 
 socket.on('dataChange', data => {
+    productBuffer=data
     tableDraw(data)
 })
 
+socket.on('error', errorData =>{
+    alert('No se puede completar la operación. Error: \n'+errorData)
+})
+
 tableDraw = (products) =>{
-    let prodList= '<table><tr> <th>Código</th> <th>Producto</th>  <th>precio</th> <th>Stock</th> <th>Acciones</th> </tr>'
+    let prodList= '<table><tr> <th>Código</th> <th>Producto</th>  <th>precio</th> <th>Stock</th> <th></th> </tr>'
 
     products.forEach(prod => {
         prodList+= '<tr>'
@@ -15,17 +21,100 @@ tableDraw = (products) =>{
          prodList+= `<td>${prod.title} </td> `
         prodList+= `<td>$${prod.price} </td> `
         prodList+= `<td>${prod.stock} </td> `
-        prodList+= `<td>    `
-        prodList+=`<a href='#' onclick='deleteItem(${prod.id})'>Borrar</a> </td> `
+        prodList+= `<td><a href='#' onclick='editItem(${prod.id})'><img src="/static/img/edit.svg" alt="editar el producto"></a>     <a href='#' onclick='deleteItem(${prod.id})'><img src="/static/img/delete.svg" alt="Borrar el producto"></a> </td>`
         prodList+= '</tr>'
     })
-   // prodList+= '</table>'
+    prodList+= '</table>'
 
     document.getElementById('productsTable').innerHTML=prodList
-    
+     
+    // Hide the form,  in case it is displayed
+    document.getElementById('ProductEditForm').innerHTML=""
+ 
+
 }
 
 deleteItem= (id) => {
     alert("Aqui pedir confirmación")
     socket.emit("deleteProduct", id)
+}
+
+editItem= (id) => {
+    console.log(id)
+    console.log(productBuffer)
+    if(id) {
+        // busco el producto con ese id.
+        prod= productBuffer.find(elem =>elem.id== id)
+        console.log(prod)
+    } else {
+        // genero un producto vacio
+        prod ={ code:"", title:"", description:"", category: "", price:0, stock:"", thumbnail:[]}
+    }
+
+    editForm= `
+    <h2>Formulario de productos</h2>
+    <form>
+        <ul>
+            <li>
+                <label for="code">Código:</label>
+                <input id="code" name="code" required value= '${prod.code}' >
+            </li>
+            <li>
+                 <label for="title">Producto:</label>
+                 <input type="text" id="title" name="title" required value= '${prod.title}' >
+            </li>
+            <li>
+                <label for="description">Descripción:</label>
+            	<input id="description" name="description" required value= '${prod.description}' >
+            </li>
+            <li>
+                <label for="price">Precio:</label>
+                <input type="text" id="price" name="price" required value=${prod.price} >
+            </li>
+            <li>
+                <label for="category">Categoría:</label>
+                <input id="category" name="category" required value= '${prod.category}' >
+            </li>
+
+            <li>
+                <label for="stock">Stock:</label>
+                <input type="text" id="stock" name="stock" required value= ${prod.stock} >
+            </li>
+            <li>
+                <label for="thumbnail">Selecciona archivo de imagen:</label>
+                <input type="file" id="thumbnail" name="thumbnail" required value= '${prod.thumbnail}' >
+            </li>
+        </ul>
+        <input type="button" id="botonCancelar" value="Cancelar">
+        <input type="button" id="botonEnviar" value="Enviar">
+    </form> `
+
+    document.getElementById('ProductEditForm').innerHTML=editForm
+    document.getElementById("botonEnviar").addEventListener("click", ()=>{sendData(id)});   
+    document.getElementById("botonCancelar").addEventListener("click", ()=>{document.getElementById('ProductEditForm').innerHTML=""
+});
+
+}
+
+sendData = (id)=>{
+    code = document.getElementById('code').value
+    title = document.getElementById('title').value
+    description = document.getElementById('description').value
+    price = document.getElementById('price').value
+    category = document.getElementById('category').value
+    thumbnail = document.getElementById('thumbnail').value
+    stock = document.getElementById('stock').value
+
+    const prodToUpdate= {code, title, description, price, category, thumbnail, stock }
+    
+    if (id) {
+        // Tengo id, edito el producto.
+        console.log("Sendata")
+
+        socket.emit('updateProduct', {prodToUpdate, id})
+    } else {
+        // No tengo id. Es producto Nuevo.
+        socket.emit('addProduct', prodToUpdate)
+    }
+    
 }
--- src/utils/socketProducts.js
+++ src/utils/socketProducts.js
@@ -7,14 +7,37 @@ const socketProducts = (io)=>{
     io.on('connection', async socket => {
         console.log('Nuevo cliente conectado')
         sendProductsChange(socket)
+        // mensaje   deleteProduct
         socket.on('deleteProduct', async id =>{
             console.log(id)
             result =await pm.deleteProductById(id)
-            if (result.status="ok"){
-                sendProductsChange()
+            if (result.status=="ok"){
+                sendProductsChange(socket)
             }
                 
         })
+        // mensaje   addProduct
+        socket.on('addProduct', async prod => {
+            result = await pm.addProduct(prod)
+            if (result.status=="ok"){
+                sendProductsChange(socket)
+            } else {
+                socket.emit('error', result.data)
+            }
+        })
+        // mensaje   editProduct
+        socket.on('updateProduct', async data => {
+            const {prodToUpdate, id} = data
+            result = await pm.updateProduct(id, prodToUpdate)
+            if (result.status=="ok"){
+                sendProductsChange(socket)
+                console.log("Producto #"+id+" actualizado")
+            } else {
+                console.log(result.status)
+                console.log(result.data)
+                socket.emit('error', result.data)
+            }
+        })        
     })
 
 }
@@ -22,7 +45,7 @@ const socketProducts = (io)=>{
 // Recover products from pm and send then to the client websocket
 const sendProductsChange = async (socket)=>{
     let result = await  pm.getProducts()
-    if (result.status='ok') {
+    if (result.status=='ok') {
         socket.emit('dataChange',result.data)
     }
 }
